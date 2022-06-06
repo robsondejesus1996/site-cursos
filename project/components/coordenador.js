4225b2346cd71be2255db2fedc1ae64086c8623e
robson <robson.jesus@unidavi.edu.br>
2022-06-06 16:08:29 -0300

    Implementação componente coordenador

--- project/components/coordenador.js
+++ project/components/coordenador.js
@@ -1,12 +1,57 @@
 import Image from "next/image";
+import styles from "../styles/Coordenador.module.css";
 
 export default function Coordenador() {
   return (
     <>
-    <h1>Componente coordenador do curso</h1>
-      <h1>Imagem do coordenador e descrição do mestre</h1>
-      <p>Descrição</p>
-      <p>--------fim componente-----------</p>
+      <section>
+        <div className={styles.div_apresentacao}>
+          <h6 className={styles.apresentacao_coordenador} data-wow-delay="0.4s">
+            Olá! Sou o Coordenador
+          </h6>
+          <span className={styles.apresentacao}>
+            {" "}
+            <p className={styles.pipe}>|</p>Augusto Fey
+          </span>
+        </div>
+
+        <div className={styles.div_informacoes_coordenador}>
+          <img className={styles.bola_cortada} src="/images/bola-cortada.png" />
+          <img
+            className={styles.figura_coordenador}
+            src="/images/coordenador-MED.png"
+          />
+          <p className={styles.p}>
+            Dr. Augusto iniciou na implantação do Residência Médica em 2001 e no
+            planejamento do curso de Medicina em 2015. Além de coordenador do
+            curso de Medicina é presidente da Comissão de Residência Médica do
+            Hospital Regional Alto Vale (COREME). Sua área de atuação médica é
+            em Cirurgia Geral, Gastrenterologia e Endoscopia Digestiva.
+          </p>
+
+          <p className={styles.separacao}>__________</p>
+
+          <p>
+            {" "}
+            <a href="" className={styles.email_coordenacao}>
+              coord.medicina@unidavi.edu.br
+            </a>{" "}
+          </p>
+          <p>
+            {" "}
+            <a href="" className={styles.contato}>
+              (47)3531-6220
+            </a>{" "}
+          </p>
+        </div>
+
+        <div>
+          <button className={styles.button}>Conheça nossos Professores</button>
+        </div>
+        <img className={styles.figura_imagem} src="/images/hello3-bg.png" />
+      </section>
+
+      <p className={styles.fim}></p>
     </>
   );
 }
