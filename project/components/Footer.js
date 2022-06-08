382e5ebdb8509f3bc022c82f06298461c6029461
robson <robson.jesus@unidavi.edu.br>
2022-06-08 11:39:05 -0300

    Implementação componente footer

--- project/components/Footer.js
+++ project/components/Footer.js
@@ -9,7 +9,29 @@ export default function Footer() {
           src="/images/logo-unidavi-muitoalemdoensino.png"
           className={styles.image}
         />
-        
+
+        <div className={styles.div_visite}>
+          <h1 className={styles.visite}>VISITE</h1>
+          <p className={styles.des_universidade}>
+            Universidade para o Desenvolvimento do Alto Vale do Itajaí - Unidavi
+          </p>
+          <p className={styles.des_endereco}>
+            Rua Dr. Guilherme Gemballa, 13 Jardim América, Rio do Sul -SC
+          </p>
+        </div>
+
+        <div className={styles.div_contate}>
+          <h1 className={styles.contate}>CONTATE</h1>
+          <p className={styles.email}>processossletivos@unidavi.edu.br</p>
+          <p className={styles.telefone}>  <i class="fa-brands fa-whatsapp"></i> (47)3531-6024</p>
+          <p className={styles.redes}>Siga </p>
+          <p className={styles.redes}>
+            <i class="fab fa-instagram">&nbsp;</i>
+            <i class="fab fa-facebook-f">&nbsp;</i>
+            <i class="fab fa-twitter">&nbsp;</i>
+            <i class="fab fa-youtube">&nbsp;</i>
+          </p>
+        </div>
       </footer>
     </>
   );
