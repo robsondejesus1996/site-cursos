e3beab640156ea359eb2e3fff83e4b173c284e9f
robson <robson.jesus@unidavi.edu.br>
2022-08-06 12:36:06 -0300

    Atualização componente accordion

--- project/components/Accordion.js
+++ project/components/Accordion.js
@@ -2,24 +2,28 @@ import React from "react";
 
 const Accordion = ({ title, active, setActive }) => {
   return (
-    <div className="accordion">
-      <div className="accordionHeading">
-        <div className="container">
-          <p>{title}</p>
-          <span onClick={() => setActive(title)}>
-            {active == title ? "X" : <i class="fa-solid fa-arrow-down"></i>}
-          </span>
+    <div className="alinhamento">
+      <div className="accordion">
+        <div className="accordionHeading">
+          <div className="container">
+            <p>{title}</p>
+            <span onClick={() => setActive(title)}>
+              <i class="fa-solid fa-building"></i>Presencial &nbsp; &nbsp;
+              <i class="fa-solid fa-clock"></i>126H &nbsp; &nbsp;
+              {active == title ? "|||" : <i class="fa-solid fa-arrow-down"></i>}
+            </span>
+          </div>
         </div>
-      </div>
 
-      <div className={(active === title ? "show" : "") + " accordionContet"}>
-        <div className="container">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
+        <div className={(active === title ? "show" : "") + " accordionContet"}>
+          <div className="container">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- project/pages/UnidadesCurriculares.js
+++ project/pages/UnidadesCurriculares.js
@@ -4,17 +4,18 @@ import Accordion from "../components/Accordion";
 import Navbar from "../components/Navbar";
 
 export default function UnidadesCurriculares() {
-  const [active, setActive] = useState("Title1");
+  const [active, setActive] = useState("Conhecimentos Gerais I");
 
   return (
     <div>
       <Navbar />
 
       <div className="espacemento">
-        <Accordion title="Title1" active={active} setActive={setActive} />
-        <Accordion title="Title2" active={active} setActive={setActive} />
-        <Accordion title="Title3" active={active} setActive={setActive} />
-        <Accordion title="Title4" active={active} setActive={setActive} />
+        <h1 className="h1">1° Fase</h1>
+        <Accordion title="Conhecimentos Gerais I" active={active} setActive={setActive} />
+        <Accordion title="Conhecimentos Gerais II" active={active} setActive={setActive} />
+        <Accordion title="Conhecimentos Gerais III" active={active} setActive={setActive} />
+        <Accordion title="Conhecimentos Gerais V" active={active} setActive={setActive} />
       </div>
     </div>
   );
