6aaac4a749e9cfc6a97229aeed19779e7d58cbf0
robson <robson.jesus@unidavi.edu.br>
2022-08-08 09:18:02 -0300

    Finalização Corpo Docente

--- project/pages/CorpoDocente.js
+++ project/pages/CorpoDocente.js
@@ -1,32 +1,50 @@
-import Link from "next/link";
-import Mensalidade from "../components/mensalidade";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import Navbar from "../components/Navbar";
+import Mensalidade from "../components/mensalidade";
+import Footer from "../components/Footer";
 import styles from "../styles/CorpoDocente.module.css";
 
 export default function CorpoDocente() {
   return (
-    <div>
+    <>
       <Navbar />
-      <div className="espacemento">
-        <section className={styles.body}>
-          <h1 className="h1">Professores do Curso</h1>
-          <p className="p">Conheça os professores do curso de Medicina</p>
-
-          <div className={styles.professores}>
-            <img
-              className={styles.figura_coordenador}
-              src="/images/coordenador-MED.png"
-            />
-            <p className={styles.p}>
-              <h1 className="h1">Doutor</h1>
+      <div className={styles.espacamento}>
+        <h1 className="h1">Professores do Curso</h1>
+        <p className={styles.p}>
+          Conheça os professores do curso de MBA em Gestão de Instituições
+          Financeiras Cooperativas
+        </p>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            height="540"
+            image="/images/coordenador-MED.png"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              <h1  className={styles.h1}>Doutor</h1>
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat.
-            </p>
-          </div>
-        </section>
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Veja Mais</Button>
+          </CardActions>
+        </Card>
+        <Mensalidade />
+        <Footer />
       </div>
-    </div>
+    </>
   );
 }
--- project/pages/UnidadesCurriculares.js
+++ project/pages/UnidadesCurriculares.js
@@ -2,21 +2,40 @@ import React, { useState } from "react";
 import Link from "next/link";
 import Accordion from "../components/Accordion";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function UnidadesCurriculares() {
   const [active, setActive] = useState("Conhecimentos Gerais I");
 
   return (
-    <div>
-      <Navbar />
+    <>
+      <div>
+        <Navbar />
 
-      <div className="espacemento">
-        <h1 className="h1">1° Fase</h1>
-        <Accordion title="Conhecimentos Gerais I" active={active} setActive={setActive} />
-        <Accordion title="Conhecimentos Gerais II" active={active} setActive={setActive} />
-        <Accordion title="Conhecimentos Gerais III" active={active} setActive={setActive} />
-        <Accordion title="Conhecimentos Gerais V" active={active} setActive={setActive} />
+        <div className="espacemento">
+          <h1 className="h1">1° Fase</h1>
+          <Accordion
+            title="Conhecimentos Gerais I"
+            active={active}
+            setActive={setActive}
+          />
+          <Accordion
+            title="Conhecimentos Gerais II"
+            active={active}
+            setActive={setActive}
+          />
+          <Accordion
+            title="Conhecimentos Gerais III"
+            active={active}
+            setActive={setActive}
+          />
+          <Accordion
+            title="Conhecimentos Gerais V"
+            active={active}
+            setActive={setActive}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
