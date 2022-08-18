eeacc0a890e0efe91828a2d0452cabdd90a2cd82
robson <robson.jesus@unidavi.edu.br>
2022-08-18 09:54:14 -0300

    Ajuste inicial corpo docente

--- project/pages/CorpoDocente.js
+++ project/pages/CorpoDocente.js
@@ -16,33 +16,79 @@ export default function CorpoDocente() {
     <>
       <Navbar />
       <div className={styles.espacamento}>
-        <h1 className="h1" data-aos="fade-up">Professores do Curso</h1>
-        <p className={styles.p} data-aos="fade-left">
-          Conheça os professores do curso de Medicina
-        </p>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardMedia
-            component="img"
-            height="540"
-            image="/images/coordenador-MED.png"
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              <h1  className={styles.h1}>Doutor</h1>
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Veja Mais</Button>
-          </CardActions>
-        </Card>
         
+        <h1 className="h1">Conheça os docentes do curso de Medicina</h1>
+
+        <div className={styles.cards_professores}>
+          <Card className={styles.card} sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              height="540"
+              image="/images/coordenador-MED.png"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                <h1 className={styles.h1}>Augusto Fey</h1>
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                <i class="fa fa-graduation-cap" aria-hidden="true"></i>Doutor
+                <br />
+                <i class="fa fa-user" aria-hidden="true"></i> Especialista{" "}
+                <br />
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Currículo Lattes</Button>
+            </CardActions>
+          </Card>
+
+          <Card className={styles.card} sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              height="540"
+              image="/images/sem_foto.png"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                <h1 className={styles.h1}>Nome Nome Nome Nome Nome</h1>
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                <i class="fa fa-graduation-cap" aria-hidden="true"></i>Doutor
+                <br />
+                <i class="fa fa-user" aria-hidden="true"></i> Especialista{" "}
+                <br />
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Currículo Lattes</Button>
+            </CardActions>
+          </Card>
+
+          <Card className={styles.card} sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              height="540"
+              image="/images/pessoafisicaman.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                <h1 className={styles.h1}>Tatiane Muniz Barbosa</h1>
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                <i class="fa fa-graduation-cap" aria-hidden="true"></i>Doutora
+                <br />
+                <i class="fa fa-user" aria-hidden="true"></i> Especialista{" "}
+                <br />
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Currículo Lattes</Button>
+            </CardActions>
+          </Card>
+        </div>
         <WhatsappFixo />
         <Footer />
       </div>
